eda19154f1bc95a3eea517a710ae0c53c6d31347
Guilherme Spati <guilherme.spati@uel.br>
2021-08-22 21:48:25 -0300

    initial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logoImg from "../../assets/logo.svg";
+import { Container, Content } from "./styles";
+
+interface headerProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({onOpenNewTransactionModal}: headerProps) {
+    return (
+        <Container>
+            <Content>
+                <img src={logoImg} alt="DT Money" />
+                <button type="button" onClick={onOpenNewTransactionModal}>
+                    Nova transação
+                </button>
+                
+            </Content>
+        </Container>
+    )
+}
